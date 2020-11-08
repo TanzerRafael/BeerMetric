3708f497e382527ab0bd41b05fe1fab2aa861a50
MarcoSupper <marco.supper2@gmail.com>
2020-11-08 19:06:49 +0100

    Added Authentification + basic searchable list

--- Frontend/ratingApp/lib/main.dart
+++ Frontend/ratingApp/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'register.dart';
+import 'signin.dart';
 
 //2
 final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -15,12 +17,11 @@ void main() async {
 
 //3
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Auth Demo',
-      home: MyHomePage(title: 'Firebase Auth Demo'),
+      title: 'Rating App',
+      home: MyHomePage(title: 'Rating App'),
     );
   }
 }
@@ -28,13 +29,23 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Text buttonText = Text(_auth.currentUser == null ? 'Sign in' : 'Sign out');
+  final duplicateItems = List<String>.generate(30, (i) => "Beer $i");
+  var items = List<String>();
+  TextEditingController editingController = TextEditingController();
 
-//4
+  @override
+  void initState() {
+    items.addAll(duplicateItems);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,21 +55,28 @@ class _MyHomePageState extends State<MyHomePage> {
           Builder(builder: (BuildContext context) {
 //5
             return FlatButton(
-              child: const Text('Sign out'),
-              textColor: Theme
-                  .of(context)
-                  .buttonColor,
+              child: buttonText,
+              textColor: Theme.of(context).buttonColor,
               onPressed: () async {
+                setState(() {
+                  _auth.currentUser;
+                });
                 final User user = _auth.currentUser;
                 if (user == null) {
-//6
-                  Scaffold.of(context).showSnackBar(const SnackBar(
-                    content: Text('No one has signed in.'),
-                  ));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignIn()),
+                  );
+                  setState(() {
+                    buttonText = Text(_auth.currentUser == null ? 'Sign in' : 'Sign out');
+                  });
                   return;
                 }
                 await _auth.signOut();
                 final String uid = user.uid;
+                setState(() {
+                  buttonText = Text(_auth.currentUser == null ? 'Sign in' : 'Sign out');
+                });
                 Scaffold.of(context).showSnackBar(SnackBar(
                   content: Text(uid + ' has successfully signed out.'),
                 ));
@@ -67,16 +85,62 @@ class _MyHomePageState extends State<MyHomePage> {
           })
         ],
       ),
-      body: Builder(builder: (BuildContext context) {
-//7
-        return ListView(
-          scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.all(16),
+      body: Container(
+        child: Column(
           children: <Widget>[
-            //TODO: UI widgets will go here.
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  filterSearchResults(value);
+                },
+                controller: editingController,
+                decoration: InputDecoration(
+                    labelText: "Search",
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${items[index]}'),
+                  );
+                },
+              ),
+            ),
           ],
-        );
-      }),
+        ),
+      ),
+
     );
+  }
+  void filterSearchResults(String query) {
+    List<String> dummySearchList = List<String>();
+    dummySearchList.addAll(duplicateItems);
+    if(query.isNotEmpty) {
+      List<String> dummyListData = List<String>();
+      dummySearchList.forEach((item) {
+        if(item.contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        items.clear();
+        items.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        items.clear();
+        items.addAll(duplicateItems);
+      });
+    }
+
   }
 }
